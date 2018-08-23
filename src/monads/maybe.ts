9585e0bede6e216dd84daf7cba8c27e5294ccde1
Patrick Michalina <patrickmichalina@mac.com>
2018-08-23 15:13:07 -0500

    fix(maybe): correct typings using NonNullable<T> (#21)

--- src/monads/maybe.ts
+++ src/monads/maybe.ts
@@ -3,13 +3,13 @@ import { IMaybe, IMaybePattern } from "../interfaces"
 export function maybe<T>(value?: T): IMaybe<T> {
   return {
     of: (x) => maybe(x),
-    valueOr: (val: T) => value === null || value === undefined ? val : value,
-    valueOrCompute: (f: () => T) => value === null || value === undefined ? f() : value,
-    tap: (obj: Partial<IMaybePattern<T, void>>) => value === null || value === undefined ? obj.none && obj.none() : obj.some && obj.some(value),
+    valueOr: (val: NonNullable<T>) => value === null || value === undefined ? val : value as NonNullable<T>,
+    valueOrCompute: (f: () => NonNullable<T>) => value === null || value === undefined ? f() : value as NonNullable<T>,
+    tap: (obj: Partial<IMaybePattern<T, void>>) => value === null || value === undefined ? obj.none && obj.none() : obj.some && obj.some(value as  NonNullable<T>),
     tapNone: (f: () => void) => (value === null || value === undefined) && f(),
-    tapSome: (f: (val: T) => void) => value !== null && value !== undefined && f(value),
-    match: <R>(pattern: IMaybePattern<T, R>) => value === null || value === undefined ? pattern.none() : pattern.some(value),
-    map: <R>(f: (t: T) => R) => value === null || value === undefined ? maybe<R>() : maybe<R>(f(value)),
-    flatMap: <R>(f: (d: T) => IMaybe<R>) => value === null || value === undefined ? maybe<R>() : f(value)
+    tapSome: (f: (val: NonNullable<T>) => void) => value !== null && value !== undefined && f(value as NonNullable<T>),
+    match: <R>(pattern: IMaybePattern<T, R>) => value === null || value === undefined ? pattern.none() : pattern.some(value as  NonNullable<T>),
+    map: <R>(f: (t: NonNullable<T>) => R) => value === null || value === undefined ? maybe<R>() : maybe<R>(f(value as NonNullable<T>)),
+    flatMap: <R>(f: (d:  NonNullable<T>) => IMaybe<R>) => value === null || value === undefined ? maybe<R>() : f(value as  NonNullable<T>)
   }
 }
