684b867f0c2d6d2ef88bcb1110993a5530725422
Will Reynolds <49566506+williamareynolds@users.noreply.github.com>
2020-02-02 22:33:43 -0600

    fix(build): correct bundle path for common and es modules (#124)

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import pkg from './package.json'
 
 export default [
   {
-    input: 'dist/lib/index.js',
+    input: 'dist/index.js',
     output: {
       name: 'monads',
       file: `dist/${pkg.main}`,
