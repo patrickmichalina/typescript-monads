07ff80b8540804e3b26fced8ff3afd1588bfccdd
Will Reynolds <49566506+williamareynolds@users.noreply.github.com>
2020-03-03 17:36:06 -0600

    feat(maybe): add a function to convert maybe to an rxjs observable (#128)
    * feat(maybe): add a function to convert maybe to an rxjs observable
    
    * chore: update package-lock

--- rollup.config.js
+++ rollup.config.js
@@ -8,14 +8,26 @@ export default [
       name: 'monads',
       file: `dist/${pkg.main}`,
       format: 'umd',
+      globals: {
+        rxjs: 'rxjs',
+        'rxjs/operators': 'rxjs/operators'
+      },
       sourcemap: true
     },
+    external: [
+      'rxjs',
+      'rxjs/operators'
+    ]
   },
   {
   input: 'src/index.ts',
   output: [
     { file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
     { file: `dist/${pkg.commonJs}`, format: 'cjs', sourcemap: true }
+  ],
+  external: [
+    'rxjs',
+    'rxjs/operators'
   ],
   plugins: [typescript()]
 }]
